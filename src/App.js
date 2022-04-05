cb24d5de21651ba69a189829af85f7c2cb796d96
MatheusPetrachin <mrp.petrachin@gmail.com>
2022-04-04 21:18:14 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import novaimagem from "./images/planta.png"
+import './App.css';
+import axios from 'axios';
+
+const App = () => {
+
+
+  const chamarAPI = (recebeu) => {
+    console.log('recebeurecebeurecebeu', recebeu)
+    const body = {
+      algumacoisa: "aqui"
+    }
+    axios.post('https://api...', body)
+  }
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={novaimagem} usemap="#planetmap"></img>
+        <>
+        <map id="planetmap" name="planetmap" >
+
+        <area shape="rect" coords="8,36,176,299" alt="suite" id="suite" onClick={() => chamarAPI("suite")} class="grab"/>
+        <area shape="rect" coords="179,4,343,233" alt="escritorio" id="escritorio"  onClick={() => chamarAPI("escritorio")} class="grab" />
+        <area shape="rect" coords="181,234,343,299" alt="corredor" id="corredor" onClick={() => chamarAPI("corredor")} class="grab"/>
+        <area shape="poly" coords="347,236,
+                                    516,236,
+                                    516,339,
+                                    540,339,
+                                    537,382,
+                                    348,382" alt="copa" id="copa" onClick={() => chamarAPI("copa")} class="grab"/>        
+        <area shape="rect" coords="347,80,512,236" alt="sala" id="sala" onClick={() => chamarAPI("sala")} class="grab"/>
+        <area shape="rect" coords="347,2,512,75" alt="varanda" id="varanda" onClick={() => chamarAPI("varanda")} class="grab"/>
+        <area shape="rect" coords="518,8,626,338" alt="cosinha" id="cosinha" onClick={() => chamarAPI("cosinha")} class="grab"/>
+        <area shape="rect" coords="10,304,176,381" alt="banh_suite" id="banh_suite" onClick={() => chamarAPI("banh_suite")} class="grab"/>
+        <area shape="rect" coords="183,304,339,381" alt="banh_suite" id="banh_suite" onClick={() => chamarAPI("banh_suite")} class="grab"/>
+        </map>        
+        </>
+      </header>
+    </div>
+  );
+}
+
+export default App;
